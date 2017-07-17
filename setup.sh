b4653d166ccd92a1972d17b28dcb81ffdfd57ff5
NiallBunting <niallbunting@googlemail.com>
2017-07-17 17:51:29 +0200

    Add script to create a mongo and container.
    This script will bring up mongo and a container.

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker run -d --name mongo_${1} mongo
+
+mongoip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mongo_${1})
+
+echo $mongoip
+sleep 10
+
+docker run -d --name dailyvideo_$1 -p 300${1}:3000 -e DAILY_VIDEO_DATABASE_URI="mongodb://$mongoip:27017" dailyvideo
+
+echo 300${1}
