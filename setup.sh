8a47eec045c9ccf9f1f1cc86db185a7e05fe77d5
NiallBunting <niallbunting@googlemail.com>
2017-07-18 15:14:08 +0200

    Make docker reboot the container unless stopped.
    This is a much better solution as it will restart the container even
    after a restart unless stopped. Therefore it protects the container more
    and means that if required you can run the container once.

--- setup.sh
+++ setup.sh
@@ -7,6 +7,6 @@ mongoip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{
 echo $mongoip
 sleep 10
 
-docker run -d --name dailyvideo_$1 -p 300${1}:3000 -e DAILY_VIDEO_DATABASE_URI="mongodb://$mongoip:27017" dailyvideo
+docker run -d --name dailyvideo_$1 -p 300${1}:3000 --restart unless-stopped -e DAILY_VIDEO_DATABASE_URI="mongodb://$mongoip:27017" dailyvideo
 
 echo 300${1}
